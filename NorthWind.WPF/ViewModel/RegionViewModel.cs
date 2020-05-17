00233c0fdb8c865cc915057b572f7eb245d4f036
Андрей Федерягин <eniluck@mail.ru>
2020-05-17 18:28:37 +0500

    Заполнение list MVVM

---
+++ NorthWind.WPF/ViewModel/RegionViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Northwind.DB.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.WPF.ViewModel
+{
+    public class RegionViewModel : INotifyPropertyChanged
+    {
+        private Region region;
+
+        public RegionViewModel(Region region)
+        {
+            this.region = region;
+        }
+
+        private int regionID;
+
+        public int RegionID
+        {
+            get { return regionID; }
+            set { 
+                regionID = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string regionDescription;
+
+        public string RegionDescription
+        {
+            get { return regionDescription; }
+            set { 
+                regionDescription = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ NorthWind.WPF/ViewModel/RegionWindowViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Northwind.DB;
+using Northwind.DB.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.WPF.ViewModel
+{
+    public class RegionWindowViewModel : INotifyPropertyChanged
+    {
+        private Region selectedRegion;
+        private RegionRepository regionRepository;
+
+        public ObservableCollection<Region> Regions { get; set; }
+
+        public Region SelectedRegion
+        {
+            get { return selectedRegion; }
+            set
+            {
+                selectedRegion = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public RegionWindowViewModel()
+        {
+            //TODO: вынести в файл настроек и чтение из Класса Northwind.BL
+            string connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True";
+            regionRepository = new RegionRepository(connString);
+            Regions = new ObservableCollection<Region>(regionRepository.GetAll());
+        }
+    }
+}
